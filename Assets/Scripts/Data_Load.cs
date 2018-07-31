3a7d299ff92a123fe600d1554a5e2b34df3af51b
JuneYeoungKim <sky14786@gmail.com>
2018-08-01 01:19:24 +0900

    Finishing work
    Finishing work

---
+++ Assets/Scripts/Data_Load.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LitJson;
+
+public class Data_Load : MonoBehaviour {
+    public string url;
+
+    private void Awake()
+    {
+        url = "sky14786.cafe24.com/Friend_Select.php";
+        this.GetComponent<Button>().onClick.AddListener(() => StartCoroutine(_SelectDB()));
+    }
+
+    IEnumerator _SelectDB()
+    {
+        WWW WebRequest = new WWW(url);
+        if (WebRequest.error == null)
+            Debug.Log("error null");
+        else
+            Debug.Log("error");
+        while (!WebRequest.isDone)
+        {
+            yield return null;
+        }
+        Debug.Log(WebRequest.text);
+        var n = LitJson.JsonMapper.ToObject(WebRequest.text);
+        //for(int i=0;i<n.Count;i++)
+        {
+            Instantiate(SystemManager.Instance.one_recorde, SystemManager.Instance.Create.transform); //<< 여기부터
+        }
+     
+
+        yield return WebRequest;
+    }
+}
--- Assets/Scripts/SystemManager.cs
+++ Assets/Scripts/SystemManager.cs
@@ -4,30 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SystemManager : MonoBehaviour {
-    public string url;
-    public InputField name, age, sex, phone, job, where, personality, etc;
-    private void Awake()
+    private static SystemManager instance;
+    public static SystemManager Instance
     {
-        url = "sky14786.cafe24.com/Insert_Friend.php";
+        get
+        {
+            if(instance==null)
+            {
+                instance = new SystemManager();
+            }
+            return instance;
+        }
     }
-
-    IEnumerator _InsertFriend()
-    {
-        WWWForm form = new WWWForm();
-
-        form.AddField("name", name.text);
-        form.AddField("age", age.text);
-        form.AddField("sex", sex.text);
-        form.AddField("phone",phone.text);
-        form.AddField("job", job.text);
-        form.AddField("where", where.text);
-        form.AddField("personality", personality.text);
-        form.AddField("etc", etc.text);
-
-        WWW WebRequest = new WWW(url, form);
-        yield return WebRequest;
-
-        Debug.Log(WebRequest.error);
-        yield break;
-    }
+    public GameObject one_recorde,Create;
+    public Text name, age, sex, phone, job, place, personality, etc, update_date;
+   
+    
+    
 }
