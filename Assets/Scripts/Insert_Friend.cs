a19b9cf807a5950dd0afd63a4b914f38da910eb8
JuneYeoungKim <sky14786@gmail.com>
2018-07-31 18:18:12 +0900

    Recommit
    Recommit

--- Assets/Scripts/Insert_Friend.cs
+++ Assets/Scripts/Insert_Friend.cs
@@ -23,46 +23,23 @@ public class Insert_Friend : MonoBehaviour {
     {
         Debug.Log("DB 정보 입력 시작");
         WWWForm form = new WWWForm();
-       
+
         Dictionary<string, string> DataDic = new Dictionary<string, string>();
         DataDic.Add("names", names.text);
         DataDic.Add("age", age.text);
         DataDic.Add("sex", sex.text);
         DataDic.Add("phone", phone.text);
         DataDic.Add("job", job.text);
-        DataDic.Add("place", where.text);
+        DataDic.Add("where", where.text);
         DataDic.Add("personality", personality.text);
         DataDic.Add("etc", etc.text);
 
-        foreach(KeyValuePair<string, string>data in DataDic)
+        foreach (KeyValuePair<string, string> data in DataDic)
         {
             form.AddField(data.Key, data.Value);
-            WebRequest = new WWW(url, form);
         }
-        //form.AddField("names", names.text);
-        //form.AddField("age", age.text);
-        //form.AddField("sex", sex.text);
-        //form.AddField("phone", phone.text);
-        //form.AddField("job", job.text);
-        //form.AddField("where", where.text);
-        //form.AddField("personality", personality.text);
-        //form.AddField("etc", etc.text);
+        WebRequest = new WWW(url, form);
 
-//        Dictionary<string, string> mapDataDic = new Dictionary<string, string>();
-//​
-//               WWWForm uploadData = new WWWForm();
-//        mapDataDic.Add("type", "login");
-//        mapDataDic.Add("email", strLoginID);
-//        mapDataDic.Add("password", strPwd);
-//​
-//               foreach (KeyValuePair<string, string> data in mapDataDic)
-//        {
-//            uploadData.AddField(data.Key, data.Value);
-//            weqLogin = new WWW(FileDownload.Instance.apiUrl, uploadData);
-//        }​
-        
-
-       
         yield return WebRequest;
 
         while (!WebRequest.isDone)
