6e60916693378d6fe47a8887819c1e8f2de1bcd1
JuneYeoungKim <sky14786@gmail.com>
2018-08-03 14:47:30 +0900

    GUI Update.... ~ing
    GUI Update.... ~ing

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonController : MonoBehaviour {
+    private void Awake()
+    {
+        if(this.name=="Input_Button")
+        {
+            this.GetComponent<Button>().onClick.AddListener(() => UIManager.Instance._InputPanelOn());
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour {
+    private static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if(instance==null)
+            {
+                instance = (UIManager)FindObjectOfType<UIManager>();
+            }
+            return instance;
+        }
+    }
+
+    public GameObject Data_Input_Panel;
+
+  public void _InputPanelOn()
+    {
+        Data_Input_Panel.SetActive(true);
+    }
+}
