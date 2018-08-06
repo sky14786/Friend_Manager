b960f9cb66ef90930c16c81eb531e0c6ef74cf8a
JuneYeoungKim <sky14786@gmail.com>
2018-08-06 17:27:06 +0900

    User System Add
    User System Add

--- Assets/Scripts/Friend_Info.cs
+++ Assets/Scripts/Friend_Info.cs
@@ -1,22 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class Friend_Info : MonoBehaviour {
+//public class Friend_Info : MonoBehaviour {
 
-    private static Friend_Info instance;
-    public static Friend_Info Instance
-    {
-        get
-        {
-            if(instance==null)
-            {
-                instance = (Friend_Info)FindObjectOfType<Friend_Info>();
-            }
-            return instance;
-        }
-    }
+//    private static Friend_Info instance;
+//    public static Friend_Info Instance
+//    {
+//        get
+//        {
+//            if(instance==null)
+//            {
+//                instance = (Friend_Info)FindObjectOfType<Friend_Info>();
+//            }
+//            return instance;
+//        }
+//    }
 
-    public Text name, age, sex, phone, job, place, personality, etc, update_date;
-  }
+//    public Text name, age, sex, phone, job, place, personality, etc, update_date;
+//  }
