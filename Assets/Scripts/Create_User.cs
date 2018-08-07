293d88136ace1323e1cee7dcc3a26b5875113d84
JuneYeoungKim <sky14786@gmail.com>
2018-08-07 14:37:16 +0900

    User Create GUI Add
    User Create GUI Add

---
+++ Assets/Scripts/Create_User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create_User : MonoBehaviour {
+    public string url;
+
+    private void Awake()
+    {
+        url = "sky14786.cafe24.com/Create_User.php";
+    }
+
+    IEnumerator _Createuser()
+    {
+
+    }
+}
