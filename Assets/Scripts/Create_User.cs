a45c8084cf0b74e4fd84ddd67d3813708eba67de
JuneYeoungKim <sky14786@gmail.com>
2018-08-08 00:48:56 +0900

    Today Work End Line

--- Assets/Scripts/Create_User.cs
+++ Assets/Scripts/Create_User.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Create_User : MonoBehaviour
 {
-    public string url, ID_Check_url,User_PW;
+    public string url, ID_Check_url, User_PW;
+    public bool isUsed, isSame;
 
     private void Awake()
     {
         url = "sky14786.cafe24.com/Create_User.php";
-       ID_Check_url = "sky14786.cafe24.com/ID_Check.php";
+        ID_Check_url = "sky14786.cafe24.com/ID_Check.php";
     }
 
     IEnumerator _Createuser()
@@ -17,9 +18,9 @@ public class Create_User : MonoBehaviour
         WWWForm form = new WWWForm();
 
         
-           
-        else
-            Debug.Log("ID는 6~12자 입니다.");
+
+
+        Debug.Log("ID는 6~12자 입니다.");
         if (UIManager.Instance.Create_PW.text.ToString().Length >= 8 && UIManager.Instance.Create_PW.text.ToString().Length <= 15)
             if (UIManager.Instance.Create_PW == UIManager.Instance.Create_PW_Check)
                 form.AddField("pw", UIManager.Instance.Create_PW.ToString());
@@ -27,7 +28,7 @@ public class Create_User : MonoBehaviour
                 Debug.Log("PW와 PW_Check가 같지 않습니다.");
         else
             Debug.Log("PW는 8~15자 입니다.");
-
+        yield break;
     }
 
     IEnumerator _ID_Check()
@@ -38,9 +39,33 @@ public class Create_User : MonoBehaviour
             Form.AddField("id", UIManager.Instance.Create_ID.text.ToString());
 
             WWW WebRequest = new WWW(ID_Check_url, Form);
+
+            yield return WebRequest;
+
+            while (!WebRequest.isDone)
+            {
+                yield return null;
+            }
+
+            if (WebRequest.text == "true")
+            {
+                UIManager.Instance.Create_ID_Check.text = "사용가능한 ID 입니다.";
+                isUsed = true;
+            }
+            else
+            {
+                UIManager.Instance.Create_ID_Check.text = "이미 존재하는 ID입니다.";
+                isUsed = false;
+            }
         }
         else
-            Debug.Log("ID는 6~12자 입니다.");
-        
+        {
+            UIManager.Instance.Create_ID_Check.text = "아이디는 6~12자 입니다.";
+            isUsed = false;
+        }
+        //여기부터
+
+
+        yield break;
     }
 }
