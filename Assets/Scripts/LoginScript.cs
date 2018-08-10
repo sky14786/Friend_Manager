294e4bce38f1d665f72a6e02f2e5f27a2a3e08ec
JuneYeoungKim <sky14786@gmail.com>
2018-08-10 17:09:07 +0900

    Login GUI Ani Add
    Login GUI Ani Add

--- Assets/Scripts/LoginScript.cs
+++ Assets/Scripts/LoginScript.cs
@@ -1,13 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LoginScript : MonoBehaviour {
+public class LoginScript : MonoBehaviour
+{
 
     public string Login_url;
-
+    public InputField User_id, User_Pw;
     private void Awake()
     {
-        Login_url = "sky14786.cafe24.com/FM/User_Login";
+        Debug.Log("Login URL Setting Complete");
+        Login_url = "sky14786.cafe24.com/FM/User_Login.php";
+
+        this.GetComponent<Button>().onClick.AddListener(() => StartCoroutine(_Login()));
+    }
+
+    IEnumerator _Login()
+    {
+        WWWForm form = new WWWForm();
+
+        form.AddField("id", User_id.text.ToString());
+        form.AddField("pw", User_Pw.text.ToString());
+
+        WWW WebReqeust = new WWW(Login_url, form);
+
+        if (WebReqeust.text == User_id.text.ToString())
+        {
+            SystemManager.Instance.User_ID = WebReqeust.text;
+            UIManager.Instance.Main_Panel.SetActive(false);
+            UIManager.Instance._MoveHomePanel();
+        }
+        else
+        {
+
+            switch (WebReqeust.text)
+            {
+                case "ID Error":
+                    {
+                        Debug.Log(WebReqeust.text);
+                        yield break;
+                    }
+                case "PW Error":
+                    {
+                        Debug.Log(WebReqeust.text);
+                        yield break;
+                    }
+            }
+        }
+
+
     }
 }
