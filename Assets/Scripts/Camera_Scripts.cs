27526ba941a4a00d7f231054b2d01bd9d835b0b2
JuneYeoungKim <sky14786@gmail.com>
2018-08-14 17:11:45 +0900

    JPG Upload System Add
    JPG Upload System Add

--- Assets/Scripts/Camera_Scripts.cs
+++ Assets/Scripts/Camera_Scripts.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using System.Net;
 
 
 
 public class Camera_Scripts : MonoBehaviour
 {
     public WebCamTexture Cam;
-    public Color32[] data;
+    public Color32[] temp2;
     public Texture2D temp;
+  
 
 
     private void Awake()
     {
+    
         Cam = new WebCamTexture();
         UIManager.Instance.CamObject.GetComponent<Renderer>().material.mainTexture = Cam;
         UIManager.Instance.CamOn_Btn.onClick.AddListener(() => CamOn());
         UIManager.Instance.CamOff_Btn.onClick.AddListener(() => CamOff());
         UIManager.Instance.CamShot_Btn.onClick.AddListener(() => Shot());
+        UIManager.Instance.CamUpLoad_Btn.onClick.AddListener(() => StartCoroutine(_CamUpload()));
     }
 
     public void CamOn()
@@ -37,13 +41,51 @@ public class Camera_Scripts : MonoBehaviour
 
     public void Shot()
     {
-        data = new Color32[Cam.width * Cam.height];
+      
         Texture2D temp = new Texture2D(Cam.width, Cam.height);
         temp.SetPixels32(Cam.GetPixels32());
-        ImageConversion.EncodeToJPG(temp);
-
-        
-        Cam.Stop();
+        Debug.Log(temp.width);
+        Debug.Log(temp.height);
+        System.IO.File.WriteAllBytes("temp.jpg", ImageConversion.EncodeToJPG(temp));
+        Cam.Stop();     
     }
-  
+
+    IEnumerator  _CamUpload()
+    {
+        FtpWebRequest ftp = (FtpWebRequest)WebRequest.Create("ftp://sky14786.cafe24.com:21/FM/Images/" + SystemManager.Instance.User_ID.ToString() + ".jpg");
+        ftp.Credentials = new NetworkCredential("sky14786", "whdkfk32!~");
+        ftp.UsePassive = true;
+        ftp.UseBinary = true;
+        ftp.KeepAlive = false;
+
+
+        byte[] data = new byte[temp.width*temp.height * 4];
+        temp2 = temp.GetPixels32();
+        int idx = 0;
+        for (int i = 0; i < temp2.Length; i++)
+        {
+            data[idx] = temp2[i].a;
+            data[idx + 1] = temp2[i].r;
+            data[idx + 2] = temp2[i].g;
+            data[idx + 3] = temp2[i].b;
+
+            idx += 4;
+        }
+
+        ftp.ContentLength = data.Length;
+        using (Stream reqStream = ftp.GetRequestStream())
+        {
+            reqStream.Write(data, 0, data.Length);
+        }
+
+        using (FtpWebResponse resp = (FtpWebResponse)ftp.GetResponse())
+        {
+            Debug.Log("Upload: {0}" + resp.StatusDescription);
+        }
+
+        yield break;
+
+    }
 }
+
+
