46e59e3f9e8fae82a23740611f2263dcbea54b88
JuneYeoungKim <sky14786@gmail.com>
2018-08-27 21:44:44 +0900

    Friend Delete System Complete
    Friend Delete System Complete

---
+++ Assets/Scripts/Friend_Delete.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Friend_Delete : MonoBehaviour {
+    public string Delete_url;
+    private void Awake()
+    {
+        Delete_url = "sky14786.cafe24.com/FM/Delete_Friend.php";
+        Debug.Log("Delete_Url Setting Complete!");
+        this.GetComponent<Button>().onClick.AddListener(() => StartCoroutine(_DeleteFriend()));
+
+      
+
+    }
+
+    IEnumerator _DeleteFriend()
+    {
+        WWWForm Form = new WWWForm();
+
+        Form.AddField("no", this.transform.parent.GetChild(0).GetComponent<Text>().text);
+
+        WWW WebRequest = new WWW(Delete_url, Form);
+        while (!WebRequest.isDone)
+        {
+            yield return null;
+        }
+
+        yield return WebRequest;
+        yield break;
+    }
+}
